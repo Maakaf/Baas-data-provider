1b82ba93e848d68652f771b537c21ac48690b782
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-05-16 00:27:47 +0300

    add functions

--- src/leaderboard/leaderboard.service.ts
+++ src/leaderboard/leaderboard.service.ts
@@ -2,11 +2,10 @@ import { AnalyticsDto } from '@/common/dto/leaderboard';
 import getLeaderBoardDataFROMJSON from '@/common/utils/getLeaderBoardDataFROMJSON';
 import { LeaderboardTypeAnalytics } from '@/types/leaderboard';
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class LeaderboardService implements OnModuleInit {
-  onModuleInit() {}
-
   async getLeaderboardData(): Promise<AnalyticsDto> {
     const since = '2024-01-05T00:00:00Z';
     const until = '2024-04-12T00:00:00Z';
@@ -15,5 +14,19 @@ export class LeaderboardService implements OnModuleInit {
       since,
       until,
     } as LeaderboardTypeAnalytics;
+  }
+
+  async onModuleInit(): Promise<void> {
+    await this.handleCron();
+  }
+
+  @Cron(CronExpression.EVERY_10_MINUTES)
+  async handleCron() {
+    // Implement a delete function to delete old data 6 weeks or older
+    await this.saveLeaderboard();
+  }
+
+  private async saveLeaderboard() {
+    // Implement this
   }
 }
