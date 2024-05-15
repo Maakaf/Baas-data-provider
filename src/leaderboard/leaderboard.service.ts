44cc854cd200a920e030d04bdff3a6dc83f41fec
barel-mishal <barel.mishal@mail.huji.ac.il>
2024-05-16 00:33:20 +0300

    add function to save to the database

--- src/leaderboard/leaderboard.service.ts
+++ src/leaderboard/leaderboard.service.ts
@@ -1,11 +1,22 @@
 import { AnalyticsDto } from '@/common/dto/leaderboard';
+import {
+  Leaderboard,
+  LeaderboardDocument,
+} from '@/common/mongoose/schemas/leaderboard';
 import getLeaderBoardDataFROMJSON from '@/common/utils/getLeaderBoardDataFROMJSON';
 import { LeaderboardTypeAnalytics } from '@/types/leaderboard';
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class LeaderboardService implements OnModuleInit {
+  constructor(
+    @InjectModel(Leaderboard.name)
+    private readonly languageModel: Model<LeaderboardDocument>,
+  ) {}
+
   async getLeaderboardData(): Promise<AnalyticsDto> {
     const since = '2024-01-05T00:00:00Z';
     const until = '2024-04-12T00:00:00Z';
